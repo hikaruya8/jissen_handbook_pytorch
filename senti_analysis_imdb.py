76109a8a16623fdc646ca7f4159056c021c9ff37
hikaru-y <>
2019-04-09 08:55:06 +0900

    Fieldのセットアップ完了

--- senti_analysis_imdb.py
+++ senti_analysis_imdb.py
@@ -25,3 +25,19 @@ LABEL = data.LabelField()
 train_dataset, test_dataset = datasets.IMDB.splits(TEXT, LABEL)
 train_dataset, val_dataset = train_dataset.split()
 
+TEXT.build_vocab(train_dataset, min_freq=3, vectors=vocab.GloVe(name='6B', dim=300)) #学習済み埋め込みベクトル glove.6B.300d.txtを使用
+LABEL.build_vocab(train_dataset) #単語の辞書を作成
+
+print(TEXT.vocab.freqs.most_common(10)) #単語件数top10
+print(LABEL.vocab.freqs) #ラベルごとの件数
+print(TEXT.vocab.itos[:10]) #単語10個
+
+#バッチ単位にする
+train_iter, val_dataset, test_dataset = data.BucketIterator.splits((train_dataset, val_dataset, test_dataset), batch_size=32, sort_key=lambda x: len(x.text), repeat=False, shuffle=True)
+
+vocab_size = len(TEXT.vocab) #単語数
+print(vocab_size) #単語数のサイズ
+
+word_embeddings = TEXT.vocab.vectors #埋め込みベクトル
+print(word_embeddings.size()) #埋め込みベクトルのサイズ
+
